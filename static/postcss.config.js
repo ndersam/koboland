c00a5c965697f973df6c6e44d09a7b56567e0769
nders <ndersamuel@gmail.com>
2019-06-18 18:10:01 +0800

    Refactoring styles

--- static/postcss.config.js
+++ static/postcss.config.js
@@ -2,6 +2,25 @@ module.exports = {
     "plugins": [
         // require('postcss-import'),
         // require('tailwindcss')('./tailwind.config.js'),
+        require('postcss-extend-rule')(),
+        require('precss')({}),
         require('autoprefixer')(),
     ]
 };
+
+// module.exports = () => ({
+//   plugins: {
+//     // 'stylelint': {
+//     //   "extends": "stylelint-config-standard",
+//     //   "rules": {
+//     //     "max-empty-lines": 2
+//     //   }
+//     // },
+//     'precss': {},
+//       'autoprefixer': {},
+//     // 'postcss-cssnext': {},
+//     // 'cssnano': {
+//     //   'autoprefixer': false
+//     // }
+//   }
+// });
