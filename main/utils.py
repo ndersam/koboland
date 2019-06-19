5698fdd9b5e2a68eee527ed13b7ecf54a3464bb2
nders <ndersamuel@gmail.com>
2019-06-19 22:46:19 +0800

    Add sort function for sorting stories....

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from io import BytesIO
 
 from PIL import Image
 from django.core.files.base import ContentFile
+from math import log
 
 
 def create_image(storage, filename, size=(100, 100), image_mode='RGB', image_format='PNG'):
@@ -18,3 +20,26 @@ def create_image(storage, filename, size=(100, 100), image_mode='RGB', image_for
         return data
     image_file = ContentFile(data.read())
     return storage.save(filename, image_file)
+
+
+"""
+SORTING
+"""
+epoch = datetime(1970, 1, 1)
+
+
+def epoch_seconds(date):
+    td = date - epoch
+    return td.days * 86400 + td.seconds + (float(td.microseconds) / 1000000)
+
+
+def score(ups, downs):
+    return ups - downs
+
+
+def hot(ups, downs, date):
+    s = score(ups, downs)
+    order = log(max(abs(s), 1), 10)
+    sign = 1 if s > 0 else -1 if s < 0 else 0
+    seconds = epoch_seconds(date) - 1134028003
+    return round(sign * order + seconds / 45000, 7)
