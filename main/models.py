9e12bd3eedee7bd9cb3ff47ddbc0c7d596d7dc5d
nders <ndersamuel@gmail.com>
2019-06-22 03:14:59 +0800

    Add RandomIdModel .... works better and migrate

--- main/models.py
+++ main/models.py
@@ -7,19 +7,17 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.core.validators import MinLengthValidator
-from django.db import IntegrityError
 from django.db import models
 from django.template.defaultfilters import pluralize
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.crypto import get_random_string
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
 from commenting.utils import render_html
 from koboland_utils import fields as model_fields
-from .validators import UsernameValidator
 from koboland_utils import models as koboland_models
+from .validators import UsernameValidator
 
 
 class Board(models.Model):
@@ -52,7 +50,7 @@ class SubmissionMedia(models.Model):
         return self.content_type.startswith('image')
 
 
-class Votable(koboland_models.BaseModel):
+class Votable(koboland_models.RandomPrimaryIdModel):
     content = models.TextField(blank=True)
     content_html = models.TextField(blank=True)
     modified = models.BooleanField(default=False)
